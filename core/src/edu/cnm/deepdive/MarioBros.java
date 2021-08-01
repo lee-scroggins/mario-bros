81396c42844dc66201c9d1018c78e4e56742f8c1
Lee Scroggins <lscrggns@yahoo.com>
2021-07-31 23:05:55 -0600

    Initial commit

---
+++ core/src/edu/cnm/deepdive/MarioBros.java
@@ -0,0 +1,25 @@
+package edu.cnm.deepdive;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MarioBros extends ApplicationAdapter {
+
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
---
+++ desktop/src/edu/cnm/deepdive/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.cnm.deepdive.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
